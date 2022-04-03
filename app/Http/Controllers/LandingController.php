a837a496d4fe6ee7b0bfed867e92cafaba07cc61
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-03 19:45:39 +0430

    create Question and Answer ui in Main Page

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -11,6 +11,17 @@ class LandingController extends Controller
     {
         $courses = Course::orderByDesc('id')->take(4)->get();
         $posts = Post::orderByDesc('id')->take(4)->get();
-        return view('landing',compact(['courses','posts']));
+
+        $id1 = Post::latest()->first()->id;
+        $id2 = Post::latest()->first()->id - 1;
+        $id3 = Post::latest()->first()->id - 2;
+        $id4 = Post::latest()->first()->id - 3;
+
+        $question1 = Post::where('id',$id1)->first();
+        $question2 = Post::where('id',$id2)->first();
+        $question3 = Post::where('id',$id3)->first();
+        $question4 = Post::where('id',$id4)->first();
+
+        return view('landing',compact(['courses','posts','question1','question2','question3','question4']));
     }
 }
