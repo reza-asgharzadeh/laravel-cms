689d0c53a9edd12b2d4295816cd000e5d931c5f3
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-09 22:38:00 +0430

    Remove Question Model and compact in LandingController

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Post;
-use App\Models\Question;
 
 class LandingController extends Controller
 {
@@ -12,17 +10,6 @@ class LandingController extends Controller
     {
         $courses = Course::orderByDesc('id')->take(4)->get();
         $posts = Post::orderByDesc('id')->take(4)->get();
-
-        $id1 = Question::latest()->first()->id;
-        $id2 = Question::latest()->first()->id - 1;
-        $id3 = Question::latest()->first()->id - 2;
-        $id4 = Question::latest()->first()->id - 3;
-
-        $question1 = Question::where('id',$id1)->first();
-        $question2 = Question::where('id',$id2)->first();
-        $question3 = Question::where('id',$id3)->first();
-        $question4 = Question::where('id',$id4)->first();
-
-        return view('landing',compact(['courses','posts','question1','question2','question3','question4']));
+        return view('landing',compact(['courses','posts']));
     }
 }
