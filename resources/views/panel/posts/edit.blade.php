ad5adb5a591fe369bfb1ef771709e6d2ac322693
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-11 23:43:44 +0430

    change ui in resources/views/panel/posts/edit.blade.php

--- resources/views/panel/posts/edit.blade.php
+++ resources/views/panel/posts/edit.blade.php
@@ -6,7 +6,9 @@
         <div class="">
             <div class="page-title">
                 <div class="title_left">
-                    <h3>عناصر فرم</h3>
+                    <h3>
+                        <small>ویرایش مقاله</small>
+                    </h3>
                 </div>
 
                 <div class="title_right">
@@ -25,8 +27,8 @@
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>طرح فرم
-                                <small>عناصر فرم های مختلف</small>
+                            <h2>
+                                <small>ویرایش مقاله سایت</small>
                             </h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
