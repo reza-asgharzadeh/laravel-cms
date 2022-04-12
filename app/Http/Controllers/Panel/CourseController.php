c5936121b5d4da76a85ec395cc4ea49e1f4e4020
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-12 21:12:13 +0430

    change index, edit and update Method in CourseController.php

--- app/Http/Controllers/Panel/CourseController.php
+++ app/Http/Controllers/Panel/CourseController.php
@@ -15,7 +15,11 @@ class CourseController extends Controller
 {
     public function index()
     {
-        $courses = Course::paginate(5);
+        if (auth()->user()->role_id === 1){
+            $courses = Course::orderByDesc('id')->paginate(5);
+        } else {
+            $courses = auth()->user()->courses()->orderByDesc('id')->paginate(5);
+        }
         return view('panel.courses.index',compact('courses'));
     }
 
@@ -35,6 +39,7 @@ class CourseController extends Controller
                 'tags' => ['برچسب یافت نشد']
             ]);
         }
+
         $data = $request->validated();
         $data['user_id'] = auth()->user()->id;
 
@@ -50,15 +55,21 @@ class CourseController extends Controller
         $category_id = $request->categories;
         $course->categories()->sync($category_id);
         $course->tags()->sync($tag_id);
+
         $request->session()->flash('status','دوره جدید با موفقیت ایجاد شد !');
         return redirect()->route('courses.index');
     }
 
     public function edit(Course $course)
     {
-        $categories = category::all();
+        $this->authorize('view', $course);
+        $courseTags = $course->tags()->pluck('id')->toArray();
         $tags = Tag::all();
-        return view('panel.courses.edit',compact(['course','categories','tags']));
+
+        $courseCategories = $course->categories()->pluck('id')->toArray();
+        $categories = Category::all();
+
+        return view('panel.courses.edit',compact('course','courseTags','tags','courseCategories','categories'));
     }
 
     public function update(UpdateCourseRequest $request, Course $course)
@@ -70,8 +81,8 @@ class CourseController extends Controller
                 'tags' => ['برچسب یافت نشد']
             ]);
         }
+
         $data = $request->validated();
-        $data['user_id'] = auth()->user()->id;
 
         if ($request->hasFile('banner')){
             $file = $request->file('banner');
@@ -87,6 +98,7 @@ class CourseController extends Controller
         $category_id = $request->categories;
         $course->categories()->sync($category_id);
         $course->tags()->sync($tag_id);
+
         $request->session()->flash('status','دوره مورد نظر با موفقیت ویرایش شد !');
         return redirect()->route('courses.index');
     }
@@ -98,81 +110,81 @@ class CourseController extends Controller
         return back();
     }
 
-    public function payment(Course $course)
-    {
-        $merchant_id = "25161ef6-aa88-4a3b-acb9-3962e080d1b7";
-        $price = $course->price * 10;
-        $course_name = $course->name;
-
-        $data = array("merchant_id" => $merchant_id,
-            "amount" => $price,
-            "callback_url" => "http://localhost:8000/payment/course/checker",
-            "description" => $course_name,
-            "metadata" => [ "email" => "info@email.com","mobile"=>"09121111111"],
-        );
-        $jsonData = json_encode($data);
-        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/request.json');
-        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($jsonData)
-        ));
-
-        $result = curl_exec($ch);
-        $err = curl_error($ch);
-        $result = json_decode($result, true, JSON_PRETTY_PRINT);
-        curl_close($ch);
-
-
-
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            if (empty($result['errors'])) {
-                if ($result['data']['code'] == 100) {
-                    return redirect('https://www.zarinpal.com/pg/StartPay/' . $result['data']["authority"]);
-                }
-            } else {
-                echo'Error Code: ' . $result['errors']['code'];
-                echo'message: ' .  $result['errors']['message'];
-            }
-        }
-    }
-
-    public function checker(Course $course)
-    {
-        $price = $course->price * 10;
-        $merchant_id = "25161ef6-aa88-4a3b-acb9-3962e080d1b7";
-        $Authority = $_GET['Authority'];
-
-        $data = array("merchant_id" => $merchant_id, "authority" => $Authority, "amount" => $price);
-        $jsonData = json_encode($data);
-        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/verify.json');
-        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v4');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($jsonData)
-        ));
-
-        $result = curl_exec($ch);
-        $err = curl_error($ch);
-        curl_close($ch);
-        $result = json_decode($result, true);
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            if ($result['data']['code'] == 100) {
-                echo 'Transation success. RefID:' . $result['data']['ref_id'];
-            } else {
-                echo'code: ' . $result['errors']['code'];
-                echo'message: ' .  $result['errors']['message'];
-            }
-        }
-    }
+//    public function payment(Course $course)
+//    {
+//        $merchant_id = "25161ef6-aa88-4a3b-acb9-3962e080d1b7";
+//        $price = $course->price * 10;
+//        $course_name = $course->name;
+//
+//        $data = array("merchant_id" => $merchant_id,
+//            "amount" => $price,
+//            "callback_url" => "http://localhost:8000/payment/course/checker",
+//            "description" => $course_name,
+//            "metadata" => [ "email" => "info@email.com","mobile"=>"09121111111"],
+//        );
+//        $jsonData = json_encode($data);
+//        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/request.json');
+//        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
+//        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//            'Content-Type: application/json',
+//            'Content-Length: ' . strlen($jsonData)
+//        ));
+//
+//        $result = curl_exec($ch);
+//        $err = curl_error($ch);
+//        $result = json_decode($result, true, JSON_PRETTY_PRINT);
+//        curl_close($ch);
+//
+//
+//
+//        if ($err) {
+//            echo "cURL Error #:" . $err;
+//        } else {
+//            if (empty($result['errors'])) {
+//                if ($result['data']['code'] == 100) {
+//                    return redirect('https://www.zarinpal.com/pg/StartPay/' . $result['data']["authority"]);
+//                }
+//            } else {
+//                echo'Error Code: ' . $result['errors']['code'];
+//                echo'message: ' .  $result['errors']['message'];
+//            }
+//        }
+//    }
+//
+//    public function checker(Course $course)
+//    {
+//        $price = $course->price * 10;
+//        $merchant_id = "25161ef6-aa88-4a3b-acb9-3962e080d1b7";
+//        $Authority = $_GET['Authority'];
+//
+//        $data = array("merchant_id" => $merchant_id, "authority" => $Authority, "amount" => $price);
+//        $jsonData = json_encode($data);
+//        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/verify.json');
+//        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v4');
+//        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//            'Content-Type: application/json',
+//            'Content-Length: ' . strlen($jsonData)
+//        ));
+//
+//        $result = curl_exec($ch);
+//        $err = curl_error($ch);
+//        curl_close($ch);
+//        $result = json_decode($result, true);
+//        if ($err) {
+//            echo "cURL Error #:" . $err;
+//        } else {
+//            if ($result['data']['code'] == 100) {
+//                echo 'Transation success. RefID:' . $result['data']['ref_id'];
+//            } else {
+//                echo'code: ' . $result['errors']['code'];
+//                echo'message: ' .  $result['errors']['message'];
+//            }
+//        }
+//    }
 }
