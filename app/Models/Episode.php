85cf5cf65d612125db55b7ef2ead8e335f6049c7
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-15 02:12:16 +0430

    change  and add display(), getCreatedAtInJalali(), getUpdatedAtInJalali() Methods and add comments() relationship in Episode.php Model

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -12,14 +12,36 @@ class Episode extends Model
     protected $fillable = [
         'name',
         'slug',
-        'price',
+        'downloadUrl',
+        'description',
         'content',
         'time',
+        'display',
         'course_id'
     ];
 
     public function course()
     {
         return $this->belongsTo(Course::class);
+    }
+
+    public function display(){
+        if($this->display == 0) return "نمایش در صورت خرید دوره" ;
+        if($this->display == 1) return "نمایش برای همه" ;
+    }
+
+    public function getCreatedAtInJalali()
+    {
+        return verta($this->created_at)->format('Y/m/d');
+    }
+
+    public function getUpdatedAtInJalali()
+    {
+        return verta($this->updated_at)->format('Y/m/d');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class,'commentable');
     }
 }
