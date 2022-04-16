a55330b75bf7fd9b8c87bde2f239feb6d09c1d7c
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-16 14:14:10 +0430

    add download_link from Payment table in app/Http/Controllers/ShowEpisodeController.php

--- app/Http/Controllers/ShowEpisodeController.php
+++ app/Http/Controllers/ShowEpisodeController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Episode;
+use App\Models\Payment;
 
 class ShowEpisodeController extends Controller
 {
     public function show(Course $course, Episode $episode){
         $most_student = $course->orderByDesc('student_count')->take(6)->get();
+        if (auth()->user()){
+            $download_link = Payment::where('user_id',auth()->user()->id)->where('course_id',$course->id)->where('status_code',100)->first();
+        } else {
+            $download_link = '';
+        }
         $episode->load(['comments' => function($query){
             return $query->where('comment_id',null)->where('is_approved',true);
         }])->loadCount('comments');
-        return view('episode',compact(['course','episode','most_student']));
+        return view('episode',compact(['course','episode','most_student','download_link']));
     }
 }
