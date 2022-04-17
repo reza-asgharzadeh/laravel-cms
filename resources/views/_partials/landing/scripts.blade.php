590ea1bc79d7e6d69e76f464e0d85878f3c6d8bf
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-17 23:08:46 +0430

    change resources/views/_partials/landing/scripts.blade.php

--- resources/views/_partials/landing/scripts.blade.php
+++ resources/views/_partials/landing/scripts.blade.php
@@ -1,2 +1,7 @@
+{{--DropDown Header Navbar--}}
 <script src="{{asset('assets/landing/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/landing/js/multidropdown.js')}}"></script>
+{{--<script src="{{asset('assets/landing/js/multidropdown.js')}}"></script>--}}
+
+{{--Cart and Episode Toggle--}}
+<script src="{{asset('assets/landing/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/landing/js/bootstrap.min.js')}}"></script>
