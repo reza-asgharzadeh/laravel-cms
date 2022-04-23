299e5798c23ca6226bb828ec0cd4b6cfdbcdcbc4
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-23 23:36:38 +0430

    add CreateOfferRequest.php and UpdateOfferRequest.php Request Validation

---
+++ app/Http/Requests/Panel/Offer/UpdateOfferRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Panel\Offer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOfferRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'type' => ['required','in:fixed,percent'],
+            'value' => ['required','integer'],
+            'course_id' => ['required', 'array'],
+            'course_id.*' => ['required', 'integer'],
+            'expiry_date' => ['required','date_format:Y-m-d H:i:s']
+        ];
+    }
+}
