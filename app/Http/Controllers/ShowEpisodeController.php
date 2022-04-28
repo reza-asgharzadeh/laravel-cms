0a1470d4858be1b9c4f17d6d3f17bd5faf5ba80f
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-28 14:38:26 +0430

    Add  and  Button for episode.blade.php in ShowEpisodeController.php

--- app/Http/Controllers/ShowEpisodeController.php
+++ app/Http/Controllers/ShowEpisodeController.php
@@ -4,20 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Episode;
-use App\Models\Payment;
+use App\Models\Order;
 
 class ShowEpisodeController extends Controller
 {
     public function show(Course $course, Episode $episode){
         $most_student = $course->orderByDesc('student_count')->take(6)->get();
-        if (auth()->user()){
-            $download_link = Payment::where('user_id',auth()->user()->id)->where('course_id',$course->id)->where('status_code',100)->first();
-        } else {
-            $download_link = '';
+
+        $downloadLink = false;
+        foreach ($course->orders as $order){
+            $downloadLink = Order::where('user_id',auth()->user()->id)->where('id',$order->id)->where('order_status',1)->first();
+            if($downloadLink){
+                $downloadLink = true;
+            }
         }
+
+        $display = false;
+
+        if(session('cart')) {
+            foreach (session('cart') as $id => $details) {
+                if ($course->id === $id) {
+                    $display = true;
+                }
+            }
+        }
+
         $episode->load(['comments' => function($query){
             return $query->where('comment_id',null)->where('is_approved',true);
         }])->loadCount('comments');
-        return view('episode',compact(['course','episode','most_student','download_link']));
+
+        return view('episode',compact(['course','episode','most_student','downloadLink','display']));
     }
 }
