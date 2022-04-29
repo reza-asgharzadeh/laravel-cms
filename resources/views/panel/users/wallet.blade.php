480665ea2a5829d4eff03a34050d20e4989e4425
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-29 12:46:22 +0430

    Add users/wallet.blade.php

---
+++ resources/views/panel/users/wallet.blade.php
@@ -0,0 +1,81 @@
+<x-panel-layout>
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>
+                        <small>کیف پول من</small>
+                    </h3>
+                </div>
+
+                <div class="title_right">
+                    <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="جست و جو برای...">
+                            <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">برو!</button>
+                    </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="col-xs-12 col-lg-8">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>
+                            <small>اطلاعات حساب</small>
+                        </h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">تنظیمات 1</a>
+                                    </li>
+                                    <li><a href="#">تنظیمات 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>نام صاحب حساب</th>
+                                    <th>موجودی حساب</th>
+                                    <th>عملیات</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{{$wallet->user->name}}</td>
+                                        <td>{{$wallet->value}}</td>
+                                        <td>
+                                            <form action="{{route('users.destroy',auth()->user()->id)}}" method="post">
+                                                @csrf
+                                                <button class="btn btn-success"><i class="fa fa-plus-circle"></i> افزایش موجودی</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <x-slot name="scripts">
+        <script src="{{asset('assets/panel/js/sweetalert2.all.min.js')}}"></script>
+    </x-slot>
+</x-panel-layout>
