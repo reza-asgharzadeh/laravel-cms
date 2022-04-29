9ee384d7d1f3592f2686c5e9e6d627c31b59e0ec
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-29 13:02:26 +0430

    Change Panel/PaymentController.php

--- app/Http/Controllers/Panel/PaymentController.php
+++ app/Http/Controllers/Panel/PaymentController.php
@@ -3,20 +3,15 @@ namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\OrderController;
-use App\Http\RandomUniqueCode;
 use App\Models\Payment;
 
 class PaymentController extends Controller
 {
     public static function storeSuccessRequest($result){
         Payment::create([
-            'user_id' => auth()->user()->id,
             'order_id' => OrderController::$order->id,
-            'amount' => OrderController::$amount,
             'authority' => $result['data']["authority"],
             'RefID' => null,
-            'order_code' => RandomUniqueCode::randomString(6),
-            'payment_type' => '0',
             'gateway_name' => 'zarinpal',
             'status_code' => $result['data']['code']
         ]);
@@ -24,13 +19,9 @@ class PaymentController extends Controller
 
     public static function storeErrorRequest($result){
         Payment::create([
-            'user_id' => auth()->user()->id,
             'order_id' => OrderController::$order->id,
-            'amount' => OrderController::$amount,
             'authority' => $result['data']["authority"],
             'RefID' => null,
-            'order_code' => RandomUniqueCode::randomString(6),
-            'payment_type' => '0',
             'gateway_name' => 'zarinpal',
             'status_code' => $result['errors']['code']
         ]);
@@ -38,13 +29,9 @@ class PaymentController extends Controller
 
     public static function storeSuccessCallBack($result,$Authority){
         Payment::create([
-            'user_id' => auth()->user()->id,
             'order_id' => OrderController::$order->id,
-            'amount' => OrderController::$amount,
             'authority' => $Authority,
             'RefID' => $result['data']['ref_id'],
-            'order_code' => RandomUniqueCode::randomString(6),
-            'payment_type' => '0',
             'gateway_name' => 'zarinpal',
             'status_code' => $result['data']['code'],
         ]);
@@ -52,13 +39,9 @@ class PaymentController extends Controller
 
     public static function storeErrorCallBack($result,$Authority){
         Payment::create([
-            'user_id' => auth()->user()->id,
             'order_id' => OrderController::$order->id,
-            'amount' => OrderController::$amount,
             'authority' => $Authority,
             'RefID' => $result['data']['ref_id'],
-            'order_code' => RandomUniqueCode::randomString(6),
-            'payment_type' => '0',
             'gateway_name' => 'zarinpal',
             'status_code' => $result['errors']['code'],
         ]);
