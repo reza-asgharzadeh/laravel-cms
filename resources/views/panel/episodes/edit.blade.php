0361155e9db51f41182a4dd4cf6ed81539ea81ba
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-29 14:26:39 +0430

    Change all edit.blade.php button to edit

--- resources/views/panel/episodes/edit.blade.php
+++ resources/views/panel/episodes/edit.blade.php
@@ -152,7 +152,7 @@
                                 <div class="ln_solid"></div>
                                 <div class="form-group">
                                     <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                        <button class="btn btn-primary">ثبت</button>
+                                        <button class="btn btn-primary">ویرایش</button>
                                         <a href="{{route('episodes.index')}}" class="btn btn-danger">انصراف</a>
                                     </div>
                                 </div>
