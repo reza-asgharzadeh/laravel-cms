fb76ac613d5ee80e2923a5ecce82ead6a469f8a7
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-04-30 17:43:06 +0430

    Add database/seeders/RoleSeeder.php

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $superAdmin = Role::create([
+            'name' => 'super-admin'
+        ]);
+
+        $superAdmin->permissions()->sync(Permission::all());
+
+        Role::create([
+           'name' => 'user'
+        ]);
+    }
+}
