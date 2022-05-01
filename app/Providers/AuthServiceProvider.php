7b58b2299e0c7db9fda179e1cab7a71275bf00a8
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-01 09:37:10 +0430

    add Gate in boot() Method and add getPermissions() Method on Providers/AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,15 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        foreach ($this->getPermissions() as $permission){
+            Gate::define($permission->name,function ($user) use ($permission){
+               return $user->role->hasPermission($permission->name);
+            });
+        }
+    }
+
+    public function getPermissions()
+    {
+        return Permission::with('roles')->get();
     }
 }
