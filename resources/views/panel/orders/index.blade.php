b30af9e143579f159d61f2dd077d7c12f6276014
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-03 20:48:48 +0430

    change order list table in panel/orders/index.blade.php

--- resources/views/panel/orders/index.blade.php
+++ resources/views/panel/orders/index.blade.php
@@ -1,94 +1,99 @@
-<x-landing-layout>
-<table class="table table-hover">
-    <thead>
-    <tr>
-        <th scope="col">ردیف</th>
-        <th scope="col">دوره ها</th>
-        <th scope="col">کد تخفیف</th>
-        <th scope="col">مبلغ کل</th>
-        <th scope="col">عملیات</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($orders as $order)
-        <tr>
-            <th scope="row">{{$loop->iteration}}</th>
-            <td>
-                <ul>
-                    @foreach($order->courses as $course)
-                        <li><img src="{{$course->getBanner()}}" alt="banner" width="50" height="50"> {{$course->name}}</li>
-                    @endforeach
-                </ul>
-            </td>
-            <td>کد تخفیف دارید؟</td>
-            <td>{{$order->total}}</td>
-            <td>
-                <div class="d-flex">
-                    <div>
-                        <form action="{{route('payment',$order->id)}}" method="post">
-                            @csrf
-                            <button class="btn btn-success"><i class="fa fa-shopping-bag"></i> پرداخت</button>
-                        </form>
-                    </div>
-                    <div class="mx-2">
-                        <form action="{{route('payment',$order->id)}}" method="post">
-                            @csrf
-                            <button class="btn btn-danger"><i class="fa fa-trash"></i> حذف</button>
-                        </form>
+<x-panel-layout>
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>
+                        <small>لیست سفارشات من</small>
+                    </h3>
+                </div>
+
+                <div class="title_right">
+                    <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="جست و جو برای...">
+                            <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">برو!</button>
+                    </span>
+                        </div>
                     </div>
                 </div>
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+            </div>
+            <div class="clearfix"></div>
 
-<x-slot name="scripts">
-    <script type="text/javascript">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>
+                            <small>سفارشات من</small>
+                        </h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">تنظیمات 1</a>
+                                    </li>
+                                    <li><a href="#">تنظیمات 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
 
-        $(".update-cart").change(function (e) {
-            e.preventDefault();
-
-            var ele = $(this);
-
-            $.ajax({
-                url: '{{ route('update.cart') }}',
-                method: "patch",
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    id: ele.parents("tr").attr("data-id"),
-                    quantity: ele.parents("tr").find(".quantity").val()
-                },
-                success: function (response) {
-                    ele.parents("tr").find("[data-th=Subtotal]").text('تومان' + response.price);
-                }
-            });
-        });
-
-        $(".remove-from-cart").click(function (e) {
-            e.preventDefault();
-
-            var ele = $(this);
-
-            $.ajax({
-                url: '{{ route('remove.from.cart') }}',
-                method: "DELETE",
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    id: ele.parents("tr").attr("data-id")
-                },
-                success: function (response) {
-                    ele.parents("tr").remove();
-                    // ele.parents("tr").find("[data-th=Total]").innerHTML = response.totals;
-                    document.getElementById("total").innerHTML = '<h3><strong>قیمت کل: ' + response.totals + ' تومان</strong></h3>'
-                    // document.getElementById("badge-total").innerHTML = response.totals;
-                    document.getElementById("basket-total").innerHTML = '<p id="basket-total">مبلغ کل: <span class="text-info">تومان' + response.totals + '</span></p>'
-                    document.getElementById("alert").innerHTML = '<p id="alert" class="alert alert-success">' + response.successs + '</p>';
-                    console.log(response.successs);
-                }
-            });
-        });
-
-    </script>
-</x-slot>
-</x-landing-layout>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th style="text-align: center">ردیف</th>
+                                    <th>دوره های این سفارش</th>
+                                    <th style="text-align: center">کد سفارش</th>
+                                    <th style="text-align: center">وضعیت سفارش</th>
+                                    <th style="text-align: center">مبلغ سفارش</th>
+                                    <th style="text-align: center">عملیات</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($orders as $order)
+                                        <tr>
+                                            <th style="text-align: center;vertical-align: middle" scope="row">{{$loop->iteration}}</th>
+                                            <td>
+                                                @foreach($order->courses as $course)
+                                                    <div style="margin-top: 1rem"><img src="{{$course->getBanner()}}" alt="" width="52" height="52"> <span>{{$course->name}}</span></div>
+                                                @endforeach
+                                            </td>
+                                            <td style="text-align: center;vertical-align: middle">{{$order->order_code}}</td>
+                                            <td style="text-align: center;vertical-align: middle" class="{{$order->order_status ? 'text-success' : 'text-danger'}}">{{$order->getOrderStatus()}}</td>
+                                            <td style="text-align: center;vertical-align: middle">{{$order->amount}}</td>
+                                            @if($order->order_status == 0)
+                                            <td style="text-align: center;vertical-align: middle">
+                                                <form action="{{route('unpaid.order.request',$order->id)}}" method="post">
+                                                    @csrf
+                                                    <button class="btn btn-success "><i class="fa fa-shopping-bag"></i> پرداخت سفارش</button>
+                                                </form>
+                                            </td>
+                                            @else
+                                                <td style="text-align: center;vertical-align: middle">
+                                                    <i class="fa fa-check text-success"></i><span> پرداخت شده</span>
+                                                </td>
+                                            @endif
+                                        </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <div class="text-center">
+                                {!! $orders->links() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-panel-layout>
