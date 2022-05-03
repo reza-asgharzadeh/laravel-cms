5679efeded8bcdf73a1775da88281c94677421f8
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-03 20:30:19 +0430

    Add ->order_status in index Method in OrderController.php

--- app/Http/Controllers/Panel/OrderController.php
+++ app/Http/Controllers/Panel/OrderController.php
@@ -5,18 +5,23 @@ namespace App\Http\Controllers\Panel;
 use App\Http\Controllers\Controller;
 use App\Http\RandomUniqueCode;
 use App\Models\Order;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class OrderController extends Controller
 {
     public static $order;
-    public static $amount;
 
-    public function index()
+    public function index(Request $request)
     {
         Gate::authorize('view-orders');
 
-        $orders = Order::paginate(5);
+        if(isset($request->order_status)) {
+            $orders = auth()->user()->orders()->where('order_status', $request->order_status)->paginate(10);
+        } else {
+            $orders = auth()->user()->orders()->paginate(10);
+        }
+
         return view('panel.orders.index',compact('orders'));
     }
 
@@ -24,13 +29,13 @@ class OrderController extends Controller
     {
         $cart = collect(session("cart"));
         $ids = $cart->keys();
-        self::$amount = session()->get("payable") ?? collect(session("cart"))->sum("price");
+        $amount = session()->get("payable") ?? collect(session("cart"))->sum("price");
         $coupon_id = session()->get("coupon_id");
 
         self::$order = Order::create([
             'user_id' => auth()->user()->id,
             'coupon_id' => $coupon_id,
-            'amount' => self::$amount,
+            'amount' => $amount,
             'order_code' => RandomUniqueCode::randomString(6),
         ]);
         self::$order->courses()->sync($ids);
