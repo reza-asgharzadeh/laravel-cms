2b4683188f498d4b6bbe219b553e10dde49afc39
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-06 22:37:54 +0430

    Add Panel/AlertController.php

---
+++ app/Http/Controllers/Panel/AlertController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Panel;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Panel\Alert\CreateAlertRequest;
+use App\Http\Requests\Panel\Alert\UpdateAlertRequest;
+use App\Models\Alert;
+use Illuminate\Http\Request;
+
+class AlertController extends Controller
+{
+    public function index()
+    {
+        $alerts = Alert::orderByDesc('id')->paginate(5);
+        return view('panel.alerts.index',compact('alerts'));
+    }
+
+    public function create()
+    {
+        return view('panel.alerts.create');
+    }
+
+    public function store(CreateAlertRequest $request)
+    {
+        $data = $request->validated();
+        Alert::create(
+            $data
+        );
+
+        $request->session()->flash('status','اطلاع رسانی جدید با موفقیت ایجاد شد !');
+        return to_route('alerts.index');
+    }
+
+    public function edit(Alert $alert)
+    {
+        return view('panel.alerts.edit',compact('alert'));
+    }
+
+    public function isApproved(Request $request, Alert $alert)
+    {
+        $alert->update([
+            'is_approved' => ! $alert->is_approved
+        ]);
+
+        $request->session()->flash('status','وضعیت اطلاع رسانی با موفقیت تغییر کرد !');
+        return to_route('alerts.index');
+    }
+
+    public function update(UpdateAlertRequest $request, Alert $alert)
+    {
+        $data = $request->validated();
+
+        if (!$request->expiry_date){
+            unset($data['expiry_date']);
+        }
+
+        $alert->update(
+            $data
+        );
+
+        $request->session()->flash('status','اطلاع رسانی مورد نظر با موفقیت ویرایش شد !');
+        return to_route('alerts.index');
+    }
+
+    public function destroy(Request $request, Alert $alert)
+    {
+        $alert->delete();
+        $request->session()->flash('status','اطلاع رسانی مورد نظر با موفقیت حذف شد !');
+        return back();
+    }
+}
