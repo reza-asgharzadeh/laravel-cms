d7e03fd4ecf60148a6d9fcf40ba7ae7fbb92b6e7
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-06 22:39:48 +0430

    Add Alert.php Model

---
+++ app/Models/Alert.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alert extends Model
+{
+    use HasFactory;
+
+    protected $dates = ['expiry_date'];
+
+    protected $fillable = [
+        'title',
+        'btn_txt',
+        'link',
+        'title_color',
+        'btn_color',
+        'btn_bg_color',
+        'btn_bg_hover_color',
+        'bg_color',
+        'expiry_date',
+        'is_approved'
+    ];
+
+    public function is_approved(){
+        return $this->is_approved ? 'فعال' : 'غیرفعال';
+    }
+}
