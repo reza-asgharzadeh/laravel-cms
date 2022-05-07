93e46f93fb7d79bb28644b785daf1954ea019e7f
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-07 22:17:20 +0430

    Add Client/CoursesController.php

---
+++ app/Http/Controllers/Client/CoursesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+
+class CoursesController extends Controller
+{
+    public function index()
+    {
+        $courses = Course::paginate(9);
+        return view('client.courses',compact('courses'));
+    }
+}
