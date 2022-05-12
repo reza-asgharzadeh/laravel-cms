57b2ffdce90790ffeb6d89722746fc3a961f5f00
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-12 23:00:50 +0430

    Add resources/views/client/page.blade.php

---
+++ resources/views/client/page.blade.php
@@ -0,0 +1,11 @@
+<x-landing-layout>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12 shadow-lg">
+                <article class="page mt-3">
+                    {!! $page->content !!}
+                </article>
+            </div>
+        </div>
+    </div>
+</x-landing-layout>
