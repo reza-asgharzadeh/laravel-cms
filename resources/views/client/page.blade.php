e099760780367d9543321a667959bfa58c0e8da4
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 14:03:43 +0430

    Add attributes to x-landing-layout in client/page.blade.php

--- resources/views/client/page.blade.php
+++ resources/views/client/page.blade.php
@@ -1,4 +1,7 @@
-<x-landing-layout>
+<x-landing-layout title="{{$page->title}}"
+                  description="{{$page->description}}"
+                  keywords="{{$page->keywords}}"
+                  pageUrl="{{route('single.page.show',$page->slug)}}">
     <div class="container mt-4">
         <div class="row">
             <div class="col-md-12 shadow-lg">
