6cdee295e0b7d4270499da73ef1ecf4be0649f5f
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 12:26:07 +0430

    Add flex column in _partials/landing/footer.blade.php

--- resources/views/_partials/landing/footer.blade.php
+++ resources/views/_partials/landing/footer.blade.php
@@ -1,21 +1,19 @@
 <a href="#" class="top"><i class="fa fa-3x fa-chevron-circle-up"></i></a>
-<footer class="shadow-lg">
-    <div class="d-flex justify-content-center mt-4">
-        <div><a class="h4 mx-3" href=""><i class="fa fa-instagram instagram-color social-icon-bg"></i></a></div>
-        <div><a class="h4 mx-3" href=""><i class="fa fa-telegram telegram-color social-icon-bg"></i></a></div>
-        <div><a class="h4 mx-3" href=""><i class="fa fa-youtube youtube-color social-icon-bg"></i></a></div>
-        <div><a class="h4 mx-3" href=""><i class="fa fa-linkedin linkedin-color social-icon-bg"></i></a></div>
-        <div><a class="h4 mx-3" href=""><i class="fa fa-twitter twitter-color social-icon-bg"></i></a></div>
+<footer class="shadow-lg d-flex justify-content-center align-items-center flex-column">
+    <div>
+        <a class="h4 mx-2" href=""><i class="fa fa-instagram instagram-color social-icon-bg"></i></a>
+        <a class="h4 mx-3" href=""><i class="fa fa-telegram telegram-color social-icon-bg"></i></a>
+        <a class="h4 mx-3" href=""><i class="fa fa-youtube youtube-color social-icon-bg"></i></a>
+        <a class="h4 mx-3" href=""><i class="fa fa-linkedin linkedin-color social-icon-bg"></i></a>
+        <a class="h4 mx-3" href=""><i class="fa fa-twitter twitter-color social-icon-bg"></i></a>
     </div>
-
-    <div class="d-flex justify-content-center mt-3">
-        <div><a class="mx-3 footer-color" href="#">درباره ما</a></div>
-        <div><a class="mx-3 footer-color" href="#">تماس با ما</a></div>
-        <div><a class="mx-3 footer-color" href="#">تبلیغات</a></div>
-        <div><a class="mx-3 footer-color" href="#">سیاست و حریم خصوصی</a></div>
+    <div class="mt-3">
+        <a class="mx-3 footer-color" href="#">درباره ما</a>
+        <a class="mx-3 footer-color" href="#">تماس با ما</a>
+        <a class="mx-3 footer-color" href="#">تبلیغات</a>
+        <a class="mx-3 footer-color" href="#">سیاست و حریم خصوصی</a>
     </div>
-
-    <div class="d-flex justify-content-center mt-3 footer-color">
-        <div><p>&copy کلیه حقوق مادی و معنوی این وب‌سایت متعلق به شرکت نوآوری دیجیتال صدرا می باشد</p></div>
+    <div class="mt-3">
+        <p>2022 &copy کلیه حقوق مادی و معنوی این وب‌سایت متعلق به رضا اصغرزاده می باشد</p>
     </div>
 </footer>
