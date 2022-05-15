3f02a07e335778f771122e00fe150a4a0ff3400c
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 14:08:59 +0430

    Add _partials/landing/meta_tag.blade.php

---
+++ resources/views/_partials/landing/meta_tag.blade.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{$title ?? env('APP_NAME')}}</title>
+<meta name="description" content="{{$description ?? ''}}"/>
+<meta name="keywords" content="{{$keywords}}" />
+<link rel="canonical" href="{{$pageUrl}}" />
