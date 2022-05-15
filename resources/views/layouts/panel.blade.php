c692d03cc40e37d053664dc30bbd7fbbec911e4a
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 14:14:32 +0430

    remove sidebar footer Glyphicons commit in layouts/panel.blade.php

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -45,19 +45,15 @@
                 <!-- /menu footer buttons -->
                 <div class="sidebar-footer hidden-small">
                     <a data-toggle="tooltip" data-placement="top" title="تنظیمات">
-{{--                        <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>--}}
                         <i class="fa fa-lg fa-cog"></i>
                     </a>
                     <a data-toggle="tooltip" data-placement="top" title="تمام صفحه" onclick="toggleFullScreen();">
-{{--                        <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>--}}
                         <i class="fa fa-lg fa-arrows-alt"></i>
                     </a>
                     <a data-toggle="tooltip" data-placement="top" title="قفل" class="lock_btn">
-{{--                        <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>--}}
                         <i class="fa fa-lg fa-eye-slash"></i>
                     </a>
                     <a data-toggle="tooltip" data-placement="top" title="خروج" href="login.html">
-{{--                        <span class="glyphicon glyphicon-off" aria-hidden="true"></span>--}}
                         <i class="fa fa-lg fa-power-off"></i>
                     </a>
                 </div>
