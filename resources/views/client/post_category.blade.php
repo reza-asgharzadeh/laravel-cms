02bdac008ebf1e0c4cf1b585c9aef9c15a2dfd65
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 14:06:19 +0430

    Add forelse empty, paginate and attributes to x-landing-layout in client/post_category.blade.php

--- resources/views/client/post_category.blade.php
+++ resources/views/client/post_category.blade.php
@@ -1,44 +1,41 @@
-<x-landing-layout>
-    <div class="container mb-5">
-        <p class="text-center h5 text-primary mb-3">دسته بندی های یافت شده:</p>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            @if(isset($posts))
-                @forelse($posts as $post)
-                <div class="col-xs-12 col-sm-6 col-lg-3">
-                    <div class="card h-100 custom-box-shadow">
-                            <img src="{{$post->getBanner()}}" class="card-img-top" alt="...">
-                            <div class="card-body text-center">
-                                <h5 class="card-title">{{$post->title}}</h5>
-                                <p class="card-text">{!! \Illuminate\Support\Str::limit($post->content, 150, $end='...') !!}</p>
-                                <a href="{{route('posts.show',$post->slug)}}" class="btn btn-more">ادامه مطلب</a>
-                            </div>
-                            <div class="card-footer text-center">
-                                <small class="text-success">{{$post->created_at}}</small>
-                            </div>
+<x-landing-layout title="همه مقالات با دسته بندی {{$category->name}}"
+                  description="در این قسمت می‌توانید تمامی مقالات با دسته بندی {{$category->name}} را مشاهده کنید"
+                  keywords="دسته بندی مقالات"
+                  pageUrl="{{route('category.post.show',$category->slug)}}">
+<div class="container mt-5">
+    <h3 class="text-center main-color h2 mb-3">تمامی مقالات با دسته بندی "{{$category->name}}"</h3>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        @forelse($posts as $post)
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="card h-100 custom-box-shadow">
+                    <a href="{{route('posts.show',$post->slug)}}"><img src="{{$post->getBanner()}}" class="card-img-top" alt="..." height="170"></a>
+                    <div class="card-body text-center">
+                        <a class="card-title text-xl" href="{{route('posts.show',$post->slug)}}">{{$post->title}}</a>
+                        <p class="card-text">{!! \Illuminate\Support\Str::limit($post->content, 60, $end='...') !!}</p>
                     </div>
-                </div>
-                @empty
-                    <p class="m-auto mt-5 text-center text-danger">مقاله ای برای این دسته بندی یافت نشد !</p>
-                @endforelse
-            @else
-                @forelse($courses as $course)
-                    <div class="col-xs-12 col-sm-6 col-lg-3">
-                        <div class="card h-100 custom-box-shadow">
-                            <img src="{{$course->getBanner()}}" class="card-img-top" alt="...">
-                            <div class="card-body text-center">
-                                <h5 class="card-title">{{$course->name}}</h5>
-                                <p class="card-text">{!! \Illuminate\Support\Str::limit($course->content, 150, $end='...') !!}</p>
-                                <a href="{{route('courses.show',$course->slug)}}" class="btn btn-more">{{$course->getPrice()}} {{$course->price == 0 ? '' : 'تومان'}}</a>
-                            </div>
-                            <div class="card-footer text-center">
-                                <small class="text-success">مدرس دوره: {{$course->user->name}}</small>
-                            </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-between">
+                            <div><p class="d-inline-block card-txt mx-1">{{$post->user->name}}</p> <img class="card-profile img-fluid" src="{{$post->user->getProfile()}}" alt="profile"></div>
+                            <div><i class="fa fa-eye text-muted"></i> <p class="d-inline-block card-txt mx-1">{{$post->view_count}} بازدید</p></div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <div><i class="fa fa-clock-o text-muted"></i> <p class="d-inline-block card-txt">زمان مطالعه: {{round(App\Http\phpCountWordPersian::string(htmlspecialchars(trim(strip_tags($post->content)))) / 215)}} دقیقه</p></div>
+                            <div><i class="fa fa-calendar text-muted"></i> <p class="d-inline-block card-txt">{{$post->created_at->diffForHumans()}}</p></div>
                         </div>
                     </div>
-                @empty
-                    <p class="m-auto mt-5 text-center text-danger">دوره ای برای این دسته بندی یافت نشد !</p>
-                @endforelse
-            @endif
-        </div>
+                </div>
+            </div>
+        @empty
+            <div style="width: 100%; min-height: 500px" class="text-center">
+                <p class="mt-5 text-danger h5">مقاله ای برای دسته بندی "{{$category->name}}" یافت نشد !</p>
+                <a class="mt-2 btn btn-video" href="{{route('blog')}}">مشاهده همه مقالات</a>
+            </div>
+        @endforelse
     </div>
+    @if(!$posts->isEmpty())
+        <div class="d-flex justify-content-center mt-5">
+            {!! $posts->links() !!}
+        </div>
+    @endif
+</div>
 </x-landing-layout>
