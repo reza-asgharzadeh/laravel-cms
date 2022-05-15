84f7f220f64c21024f0e4d02689adae90c74c11d
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 12:05:35 +0430

    Add paginate() method for  variable pagination in Client/ShowOfferPageController.php

--- app/Http/Controllers/Client/ShowOfferPageController.php
+++ app/Http/Controllers/Client/ShowOfferPageController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers\Client;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 use App\Http\Controllers\Controller;
 use App\Models\Offer;
 use Carbon\Carbon;
 
 class ShowOfferPageController extends Controller
 {
+    //create pagination from array
+    public function paginate($items, $perPage = 5, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
+
     public function show()
     {
         $offers = Offer::with('courses')
@@ -20,6 +32,8 @@ class ShowOfferPageController extends Controller
                 $courses[] = $course;
             }
         }
+
+        $courses = $this->paginate($courses,9);
 
         return view('client.offer_page',compact('courses'));
     }
