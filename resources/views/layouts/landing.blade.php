ef47736f01b63c78efdbeef76dcb026d8ca9ec2c
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-15 14:08:31 +0430

    Add @include('_partials.landing.meta_tag') in layouts/landing.blade.php

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    @include('_partials.landing.meta_tag')
     @include('_partials.landing.links')
     {{$links ?? ''}}
 </head>
