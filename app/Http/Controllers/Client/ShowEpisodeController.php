f93a1f28d6c239758ababb3b4d02d43933363fb6
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-16 18:15:35 +0430

    Add ->is_approved condition and where('is_approved',true) in Client/ShowEpisodeController.php

--- app/Http/Controllers/Client/ShowEpisodeController.php
+++ app/Http/Controllers/Client/ShowEpisodeController.php
@@ -10,30 +10,33 @@ use App\Models\Order;
 class ShowEpisodeController extends Controller
 {
     public function show(Course $course, Episode $episode){
-        $most_student = $course->orderByDesc('student_count')->take(6)->get();
+        if($course->is_approved){
+            $most_student = $course->where('is_approved',true)->orderByDesc('student_count')->take(6)->get();
 
-        $downloadLink = false;
-        foreach ($course->orders as $order){
-            $downloadLink = Order::where('user_id',auth()->user()->id)->where('id',$order->id)->where('order_status',1)->first();
-            if($downloadLink){
-                $downloadLink = true;
-            }
-        }
-
-        $display = false;
-
-        if(session('cart')) {
-            foreach (session('cart') as $id => $details) {
-                if ($course->id === $id) {
-                    $display = true;
+            $downloadLink = false;
+            foreach ($course->orders as $order){
+                $downloadLink = Order::where('user_id',auth()->user()->id)->where('id',$order->id)->where('order_status',1)->first();
+                if($downloadLink){
+                    $downloadLink = true;
                 }
             }
+
+            $display = false;
+
+            if(session('cart')) {
+                foreach (session('cart') as $id => $details) {
+                    if ($course->id === $id) {
+                        $display = true;
+                    }
+                }
+            }
+
+            $episode->load(['comments' => function($query){
+                return $query->where('comment_id',null)->where('is_approved',true);
+            }])->loadCount('comments');
+
+            return view('client.episode',compact(['course','episode','most_student','downloadLink','display']));
         }
-
-        $episode->load(['comments' => function($query){
-            return $query->where('comment_id',null)->where('is_approved',true);
-        }])->loadCount('comments');
-
-        return view('client.episode',compact(['course','episode','most_student','downloadLink','display']));
+        abort(404);
     }
 }
