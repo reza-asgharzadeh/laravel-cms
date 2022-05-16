6f18862b45594051f6161dfcbec504baeb45fbba
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-16 18:13:28 +0430

    Add ->is_approved condition and where('is_approved',true) in Client/ShowCourseController.php

--- app/Http/Controllers/Client/ShowCourseController.php
+++ app/Http/Controllers/Client/ShowCourseController.php
@@ -10,37 +10,40 @@ class ShowCourseController extends Controller
 {
     public function show(Course $course)
     {
-        $course->increment('view_count');
-        $most_student = $course->orderByDesc('student_count')->take(6)->get();
-        $course->load(['comments' => function($query){
-           return $query->where('comment_id',null)->where('is_approved',true);
-        }])->loadCount('comments');
+        if($course->is_approved){
+            $course->increment('view_count');
+            $most_student = $course->where('is_approved',true)->orderByDesc('student_count')->take(6)->get();
+            $course->load(['comments' => function($query){
+                return $query->where('comment_id',null)->where('is_approved',true);
+            }])->loadCount('comments');
 
-        $courseTime = $course->episodes()->sum('time');
+            $courseTime = $course->episodes()->sum('time');
 
-        $display = false;
+            $display = false;
 
-        if(session('cart')) {
-            foreach (session('cart') as $id => $details) {
-                if ($course->id === $id) {
-                    $display = true;
-                }
-            }
-        }
-
-        $registeredButton = false;
-
-        if (auth()->user()){
-            $orders = Order::where('user_id',auth()->user()->id)->where('order_status',1)->get();
-            foreach ($orders as $order){
-                foreach ($order->courses as $myCourse){
-                    if ($myCourse->id == $course->id){
-                        $registeredButton = true;
+            if(session('cart')) {
+                foreach (session('cart') as $id => $details) {
+                    if ($course->id === $id) {
+                        $display = true;
                     }
                 }
             }
-        }
 
-        return view('client.course',compact(['course','most_student','courseTime','display','registeredButton']));
+            $registeredButton = false;
+
+            if (auth()->user()){
+                $orders = Order::where('user_id',auth()->user()->id)->where('order_status',1)->get();
+                foreach ($orders as $order){
+                    foreach ($order->courses as $myCourse){
+                        if ($myCourse->id == $course->id){
+                            $registeredButton = true;
+                        }
+                    }
+                }
+            }
+
+            return view('client.course',compact(['course','most_student','courseTime','display','registeredButton']));
+        }
+        abort(404);
     }
 }
