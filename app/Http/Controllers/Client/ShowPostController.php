afb2283dd27982161b6aa0b8d6f283d75764542b
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-16 18:18:01 +0430

    Add ->is_approved condition and where('is_approved',true) in Client/ShowPostController.php

--- app/Http/Controllers/Client/ShowPostController.php
+++ app/Http/Controllers/Client/ShowPostController.php
@@ -9,11 +9,14 @@ class ShowPostController extends Controller
 {
     public function show(Post $post)
     {
-        $post->increment('view_count');
-        $most_visited = Post::orderByDesc('view_count')->take(6)->get();
-        $post->load(['comments' => function($query){
-            return $query->where('comment_id',null)->where('is_approved',true);
-        }])->loadCount('comments');
-        return view('client.post',compact(['post','most_visited']));
+        if($post->is_approved){
+            $post->increment('view_count');
+            $most_visited = Post::where('is_approved',true)->orderByDesc('view_count')->take(6)->get();
+            $post->load(['comments' => function($query){
+                return $query->where('comment_id',null)->where('is_approved',true);
+            }])->loadCount('comments');
+            return view('client.post',compact(['post','most_visited']));
+        }
+        abort(404);
     }
 }
