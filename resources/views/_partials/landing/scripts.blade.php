9dfa360f0422136490ea86217488355125aa48f4
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-18 22:00:51 +0430

    Add popper.min.js, navbar-toggler-jquery.sticky.js and navbar-toggler-main.js scripts in _partials/landing/scripts.blade.php

--- resources/views/_partials/landing/scripts.blade.php
+++ resources/views/_partials/landing/scripts.blade.php
@@ -2,9 +2,14 @@
 <script src="{{asset('assets/landing/js/bootstrap.bundle.min.js')}}"></script>
 {{--<script src="{{asset('assets/landing/js/multidropdown.js')}}"></script>--}}
 
-{{--Cart and Episode Toggle--}}
+{{--Cart, Panel and Episode Toggle--}}
 <script src="{{asset('assets/landing/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/landing/js/popper.min.js')}}"></script>
+
 <script src="{{asset('assets/landing/js/bootstrap.min.js')}}"></script>
 @if($alert)
     <script src="{{asset('/assets/landing/js/countdown.js')}}"></script>
 @endif
+
+<script src="{{asset('/assets/landing/js/navbar-toggler-jquery.sticky.js')}}"></script>
+<script src="{{asset('/assets/landing/js/navbar-toggler-main.js')}}"></script>
