9dcf8af6642efaee37cadc7be73157eb2c56f32d
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-19 22:40:00 +0430

    Add  and  in Client/ShowPostController.php

--- app/Http/Controllers/Client/ShowPostController.php
+++ app/Http/Controllers/Client/ShowPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use App\Models\Post;
 
 class ShowPostController extends Controller
@@ -12,10 +13,19 @@ class ShowPostController extends Controller
         if($post->is_approved){
             $post->increment('view_count');
             $most_visited = Post::where('is_approved',true)->orderByDesc('view_count')->take(6)->get();
+            $most_student = Course::where('is_approved',true)->orderByDesc('student_count')->take(6)->get();
+
             $post->load(['comments' => function($query){
                 return $query->where('comment_id',null)->where('is_approved',true);
             }])->loadCount('comments');
-            return view('client.post',compact(['post','most_visited']));
+
+            $relatedPosts = $post->whereHas('tags', function ($q) use ($post) {
+                return $q->whereIn('name', $post->tags->pluck('name'));
+            })
+                ->where('id', '!=', $post->id) // So you won't fetch same post
+                ->get();
+
+            return view('client.post',compact(['post','most_student','most_visited','relatedPosts']));
         }
         abort(404);
     }
