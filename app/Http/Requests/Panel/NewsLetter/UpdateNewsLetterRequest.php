36cd95cb68506c27f6792798d93a22ac1c14a11e
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-05-25 22:09:02 +0430

    Add Panel/NewsLetter/CreateNewsLetterRequest.php and Panel/NewsLetter/UpdateNewsLetterRequest.php

---
+++ app/Http/Requests/Panel/NewsLetter/UpdateNewsLetterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Panel\NewsLetter;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateNewsLetterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $newsletter = $this->route('newsletter');
+        return [
+            'email' => ['required', 'string', 'max:255', Rule::unique('news_letters')->ignore($newsletter->id)],
+        ];
+    }
+}
