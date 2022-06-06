d42ee249329f52c54b2f79c1c9a19a4ea67ff192
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-06 22:42:56 +0430

    Add Chapters Collapse in _partials/landing/chapters.blade.php

---
+++ resources/views/_partials/landing/chapters.blade.php
@@ -0,0 +1,31 @@
+<div class="custom-box-shadow p-3">
+    <div class="h5 text-purple mb-2">سرفصل‌ها:</div>
+    <div class="accordion" id="accordionExample">
+        @forelse($course->chapters as $chapter)
+            <div class="accordion-item mb-2 border-0">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed bg-sky rounded-4" type="button" data-bs-toggle="collapse" data-bs-target="#chapter{{$loop->iteration}}" aria-expanded="false" aria-controls="collapseTwo">
+                        {{$chapter->name}}
+                    </button>
+                </h2>
+                <div id="chapter{{$loop->iteration}}" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    @forelse($chapter->episodes as $episode)
+                        <div class="accordion-body bg-light">
+                            <div class="d-flex">
+                                <div class="p-2"><span class="circle-number">{{$loop->iteration}}</span> {{$episode->name}}</div>
+                                <div class="ms-auto p-2">{{$episode->time}} دقیقه</div>
+                                <div class="btn-video"><small><a href="{{route('episodes.show',[$episode->course,$episode->slug])}}" target="_blank"><i class="fa fa-eye"></i> مشاهده و دانلود</a></small></div>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="accordion-body bg-light">
+                            <div class="alert alert-danger" role="alert">در حال حاضر جلسه‌ای برای این فصل ایجاد نشده است.</div>
+                        </div>
+                    @endforelse
+                </div>
+            </div>
+        @empty
+            <div class="alert alert-danger" role="alert">در حال حاضر سرفصلی برای این دوره ایجاد نشده است.</div>
+        @endforelse
+    </div>
+</div>
