65a0cc17c20829d0251f64fdfb2f854c364a97a3
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-07 19:28:17 +0430

    Add client/emails/contact_us.blade.php

---
+++ resources/views/client/emails/contact_us.blade.php
@@ -0,0 +1,8 @@
+<x-email-layout>
+    <div>
+        <h1>پیام ارسال شده از {{$name}}</h1>
+        <h2>ایمیل ارسال کننده: {{$email}}</h2>
+        <h3>متن پیام:</h3>
+        <p>{{$content}}</p>
+    </div>
+</x-email-layout>
