108c7f6742b1831c9e1824cf87207340add2d3da
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-07 19:58:39 +0430

    Add </div> close tag in panel/newsletters/edit.blade.php

--- resources/views/panel/newsletters/edit.blade.php
+++ resources/views/panel/newsletters/edit.blade.php
@@ -61,6 +61,7 @@
                                     @error('email')
                                     <p class="text-danger">{{$message}}</p>
                                     @enderror
+                                </div>
                                 <div class="ln_solid"></div>
                                 <div class="form-group">
                                     <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
