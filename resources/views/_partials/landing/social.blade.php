4e10e0bff73253ea4f79da94069243f720e893fe
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-07 19:39:53 +0430

    Add _partials/landing/social.blade.php

---
+++ resources/views/_partials/landing/social.blade.php
@@ -0,0 +1,5 @@
+<a class="h4 mx-2" href=""><i class="fa fa-instagram instagram-color social-icon-bg"></i></a>
+<a class="h4 mx-3" href=""><i class="fa fa-telegram telegram-color social-icon-bg"></i></a>
+<a class="h4 mx-3" href=""><i class="fa fa-youtube youtube-color social-icon-bg"></i></a>
+<a class="h4 mx-3" href=""><i class="fa fa-linkedin linkedin-color social-icon-bg"></i></a>
+<a class="h4 mx-3" href=""><i class="fa fa-twitter twitter-color social-icon-bg"></i></a>
