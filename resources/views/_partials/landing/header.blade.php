337d78550e88432c1a33c14a1fe7e9311e2cccb1
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-08 13:15:55 +0430

    Change Header UI and Add Search Form in _partials/landing/header.blade.php

--- resources/views/_partials/landing/header.blade.php
+++ resources/views/_partials/landing/header.blade.php
@@ -9,59 +9,27 @@
 </div>
 
 
-<header class="site-navbar site-navbar-target shadow p-3" role="banner">
-    <nav class="site-navigation position-relative text-right" role="navigation">
-        <div class="container">
+<header class="site-navbar site-navbar-target p-3" role="banner">
+    <div class="container">
+        <nav class="site-navigation position-relative bg-white shadow p-4 rounded-4" role="navigation">
             <div class="d-flex justify-content-between align-items-center">
                 <div>
-                    <a class="navbar-brand m-0" href="{{route('landing')}}"><img src="{{asset('assets/landing/img/logo.png')}}" alt="logo"></a>
-                    <ul class="site-menu main-menu js-clone-nav d-none d-lg-inline-block m-0 p-0">
-                        <li><a href="{{route('landing')}}" class="nav-link">خانه</a></li>
-                        <li><a href="{{route('blog')}}" class="nav-link">مقالات</a></li>
-                        <li><a href="{{route('courses')}}" class="nav-link">دوره ها</a></li>
-                        <li class="has-children">
-                            <a href="#" class="nav-link">دسته بندی مقالات</a>
-                            <ul class="dropdown">
-                                @foreach($categories as $category)
-                                    @if(!$category->posts->isEmpty())
-                                    <li class="{{!$category->children->isEmpty() ? 'has-children' : ''}}">
-                                        <a href="{{route('category.post.show',$category->slug)}}">{{$category->name}}</a>
-                                        @foreach($category->children as $child)
-                                            <ul class="dropdown">
-                                                <li><a href="{{route('category.post.show',$child->slug)}}">{{$child->name}}</a></li>
-                                            </ul>
-                                        @endforeach
-                                    </li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#" class="nav-link">دسته بندی دوره ها</a>
-                            <ul class="dropdown">
-                                @foreach($categories as $category)
-                                    @if(!$category->courses->isEmpty())
-                                    <li class="{{!$category->children->isEmpty() ? 'has-children' : ''}}">
-                                        <a href="{{route('category.course.show',$category->slug)}}">{{$category->name}}</a>
-                                        @foreach($category->children as $child)
-                                            <ul class="dropdown">
-                                                <li><a href="{{route('category.course.show',$child->slug)}}">{{$child->name}}</a></li>
-                                            </ul>
-                                        @endforeach
-                                    </li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        </li>
-                        <li class="has-children">
-                            <a href="#" class="nav-link">لینک های مفید</a>
-                            <ul class="dropdown">
-                                <li><a href="{{route('about')}}" class="nav-link">درباره ما</a></li>
-                                <li><a href="{{route('contact')}}" class="nav-link">تماس با ما</a></li>
-                            </ul>
-                        </li>
+                    <a class="navbar-brand m-0 text-dark" href="{{route('landing')}}"><img src="{{asset('assets/landing/img/logo.png')}}" alt="logo"></a>
+                </div>
 
-                        @auth
+                <div class="w-50 js-clone-nav d-none d-lg-inline-block">
+                    <form action="{{route('search')}}">
+                        <button class="btn btn-outline-secondary border-0 position-absolute">
+                            <i class="fa fa-search"></i>
+                        </button>
+                        <input type="text" class="form-control px-5 bg-light" name="search" placeholder="دنبال چی میگردی؟">
+                        <input type="hidden" name="type" value="post">
+                    </form>
+                </div>
+
+                @auth
+                    <div>
+                        <ul class="site-menu main-menu js-clone-nav d-none d-lg-inline-block mx-5">
                             <div class="btn-group dropdown">
                                 <button type="button" class="btn-cart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <i class="fa fa-shopping-cart" aria-hidden="true"></i> سبد خرید <span id="count-basket" class="badge bg-danger">{{ count((array) session('cart')) }}</span>
@@ -100,26 +68,23 @@
                                     </div>
                                 </div>
                             </div>
-                        @endauth
-                    </ul>
-                </div>
-
-                @auth
-                    <div class="btn-group">
-                        <button type="button" class="dropdown-toggle border-0 bg-body" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{auth()->user()->name}}
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <a class="dropdown-item h6" href="{{route('panel')}}"><i class="fa fa-dashboard me-1"></i>پنل کاربری</a>
-                            <a class="dropdown-item h6" href="{{route('my.courses.index')}}"><i class="fa fa-graduation-cap me-1"></i>دوره های من</a>
-                            <a class="dropdown-item h6" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-out me-1"></i>خروج</a>
-                            <form action="{{route('logout')}}" method="post" id="logout-form">
-                                @csrf
-                            </form>
-                        </div>
-                        <span class="d-lg-none mx-3">
+                        </ul>
+                        <div class="btn-group">
+                            <button type="button" class="dropdown-toggle border-0 bg-body" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{auth()->user()->name}}
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item h6" href="{{route('panel')}}"><i class="fa fa-dashboard me-1"></i>پنل کاربری</a>
+                                <a class="dropdown-item h6" href="{{route('my.courses.index')}}"><i class="fa fa-graduation-cap me-1"></i>دوره های من</a>
+                                <a class="dropdown-item h6" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-out me-1"></i>خروج</a>
+                                <form action="{{route('logout')}}" method="post" id="logout-form">
+                                    @csrf
+                                </form>
+                            </div>
+                            <span class="d-lg-none mx-3">
                             <a href="#" class="site-menu-toggle js-menu-toggle"><i class="fa fa-lg fa-bars"></i></a>
                         </span>
+                        </div>
                     </div>
                 @else
                     <div>
@@ -131,6 +96,56 @@
                     </div>
                 @endauth
             </div>
-        </div>
-    </nav>
+        </nav>
+
+        <nav class="site-navigation position-relative text-center m-auto w-93 bg-second-nav shadow p-lg-2 rounded-second-nav" role="navigation">
+            <ul class="site-menu main-menu js-clone-nav d-none d-lg-inline-block m-0 p-0">
+                <li><a href="{{route('landing')}}" class="nav-link">خانه</a></li>
+                <li><a href="{{route('blog')}}" class="nav-link">مقالات</a></li>
+                <li><a href="{{route('courses')}}" class="nav-link">دوره ها</a></li>
+                <li class="has-children">
+                    <a href="#" class="nav-link">دسته بندی مقالات</a>
+                    <ul class="dropdown">
+                        @foreach($categories as $category)
+                            @if(!$category->posts->isEmpty())
+                                <li class="{{!$category->children->isEmpty() ? 'has-children' : ''}}">
+                                    <a href="{{route('category.post.show',$category->slug)}}">{{$category->name}}</a>
+                                    @foreach($category->children as $child)
+                                        <ul class="dropdown">
+                                            <li><a href="{{route('category.post.show',$child->slug)}}">{{$child->name}}</a></li>
+                                        </ul>
+                                    @endforeach
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="#" class="nav-link">دسته بندی دوره ها</a>
+                    <ul class="dropdown">
+                        @foreach($categories as $category)
+                            @if(!$category->courses->isEmpty())
+                                <li class="{{!$category->children->isEmpty() ? 'has-children' : ''}}">
+                                    <a href="{{route('category.course.show',$category->slug)}}">{{$category->name}}</a>
+                                    @foreach($category->children as $child)
+                                        <ul class="dropdown">
+                                            <li><a href="{{route('category.course.show',$child->slug)}}">{{$child->name}}</a></li>
+                                        </ul>
+                                    @endforeach
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </li>
+                <li class="has-children">
+                    <a href="#" class="nav-link">لینک های مفید</a>
+                    <ul class="dropdown">
+                        <li><a href="{{route('about')}}" class="nav-link">درباره ما</a></li>
+                        <li><a href="{{route('contact')}}" class="nav-link">تماس با ما</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
 </header>
