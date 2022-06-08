f2ad52b3a1aafec0d7b5267fc13fb816b8729316
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-08 12:49:36 +0430

    Remove assets/landing/css/multidropdown.css in _partials/landing/links.blade.php

--- resources/views/_partials/landing/links.blade.php
+++ resources/views/_partials/landing/links.blade.php
@@ -2,6 +2,5 @@
 <link rel="stylesheet" href="{{asset('assets/landing/css/style.css')}}">
 <link rel="stylesheet" href="{{asset('assets/landing/css/fonts.css')}}">
 <link rel="stylesheet" href="{{asset('assets/landing/css/font-awesome.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/landing/css/multidropdown.css')}}">
 <link rel="stylesheet" href="{{asset('assets/landing/css/cart.css')}}">
 <link rel="stylesheet" href="{{asset('assets/landing/css/bootstrap-navbar.css')}}">
