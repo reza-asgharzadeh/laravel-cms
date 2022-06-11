509426ad2f4ea317dc4d311cd91ad9ab64475201
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-11 20:49:59 +0430

    Integrate Episode and Chapters with course, Offers with Coupon and activities with profile and Some changes in the names in _partials/panel/sidebar.blade.php

--- resources/views/_partials/panel/sidebar.blade.php
+++ resources/views/_partials/panel/sidebar.blade.php
@@ -8,13 +8,10 @@
                 <li><a href="{{route('landing')}}" target="_blank"><i class="fa fa-eye"></i> مشاهده سایت</a></li>
             @endcan
             @can('display-edit-profile')
-                <li><a href="{{route('profiles.index')}}"><i class="fa fa-user"></i> ویرایش پروفایل</a></li>
-            @endcan
-            @can('display-activities')
-                <li><a href="{{route('activities.index')}}"><i class="fa fa-calendar"></i> فعالیت های من</a></li>
+                <li><a href="{{route('account.information')}}"><i class="fa fa-user"></i> اطلاعات پروفایل</a></li>
             @endcan
 
-                <li><a href="{{route('my.courses.index')}}"><i class="fa fa-graduation-cap"></i> دوره های من</a></li>
+                <li><a href="{{route('my.courses.index')}}"><i class="fa fa-graduation-cap"></i> دوره‌های من</a></li>
 
             @can('display-wallet')
                 <li><a href="{{route('wallets.index')}}"><i class="fa fa-shopping-bag"></i> کیف پول</a></li>
@@ -28,10 +25,10 @@
                 </li>
             @endcan
             @can('display-tickets')
-                <li><a><i class="fa fa-ticket"></i> تیکت ها <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-ticket"></i> تیکت‌ها <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                         <li><a href="{{route('tickets.create')}}">ایجاد تیکت جدید</a></li>
-                        <li><a href="{{route('tickets.index')}}">مدیریت تیکت ها</a></li>
+                        <li><a href="{{route('tickets.index')}}">مدیریت تیکت‌ها</a></li>
                     </ul>
                 </li>
             @endcan
@@ -51,10 +48,10 @@
                     </ul>
                 </li>
             @endcan
-                <li><a><i class="fa fa-file"></i> صفحات تکی <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-file"></i> صفحه سایت <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="{{route('pages.create')}}">ایجاد صفحه تکی جدید</a></li>
-                        <li><a href="{{route('pages.index')}}">مدیریت صفحات تکی</a></li>
+                        <li><a href="{{route('pages.create')}}">ایجاد صفحه جدید</a></li>
+                        <li><a href="{{route('pages.index')}}">مدیریت صفحات</a></li>
                     </ul>
                 </li>
             @can('display-posts')
@@ -68,34 +65,21 @@
             @can('display-courses')
             <li><a><i class="fa fa-graduation-cap"></i> دوره‌ها <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
-                    <li><a href="{{route('courses.create')}}">ایجاد دوره جدید</a></li>
-                    <li><a href="{{route('courses.index')}}">مدیریت دوره ها</a></li>
-                </ul>
-            </li>
-            @endcan
-            <li><a><i class="fa fa-plus"></i> فصل‌ها <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="{{route('chapters.create')}}">ایجاد فصل جدید</a></li>
-                    <li><a href="{{route('chapters.index')}}">مدیریت فصل‌ها</a></li>
-                </ul>
-            </li>
-            @can('display-episodes')
-            <li><a><i class="fa fa-television"></i> جلسات فصل <span class="fa fa-chevron-down"></span></a>
-                <ul class="nav child_menu">
-                    <li><a href="{{route('episodes.create')}}">ایجاد جلسه جدید</a></li>
-                    <li><a href="{{route('episodes.index')}}">مدیریت جلسات</a></li>
+                    <li><a href="{{route('courses.index')}}">ایجاد و مدیریت دوره‌ها</a></li>
+                    <li><a href="{{route('chapters.index')}}">ایجاد و مدیریت فصل‌های دوره</a></li>
+                    <li><a href="{{route('episodes.index')}}">ایجاد و مدیریت جلسات فصل</a></li>
                 </ul>
             </li>
             @endcan
             @can('display-categories')
-                <li><a><i class="fa fa-list-alt"></i> دسته بندی ها <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-list-alt"></i> دسته‌بندی‌ها <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="{{route('categories.index')}}">ایجاد و مدیریت دسته بندی</a></li>
+                        <li><a href="{{route('categories.index')}}">ایجاد و مدیریت دسته‌بندی</a></li>
                     </ul>
                 </li>
             @endcan
             @can('display-tags')
-            <li><a><i class="fa fa-tags"></i> برچسب ها <span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-tags"></i> برچسب‌ها <span class="fa fa-chevron-down"></span></a>
                 <ul class="nav child_menu">
                     <li><a href="{{route('tags.index')}}">ایجاد و مدیریت برچسب</a></li>
                 </ul>
@@ -113,30 +97,22 @@
                         <li><a href="{{route('contact-us.index')}}">پاسخ و مدیریت تماس‌ها</a></li>
                     </ul>
                 </li>
-                <li><a><i class="fa fa-comments"></i> خبرنامه‌ها <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-newspaper-o"></i> خبرنامه‌ها <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                         <li><a href="{{route('newsletters.index')}}">مدیریت خبرنامه‌ها</a></li>
                     </ul>
                 </li>
-                <li><a><i class="fa fa-percent"></i> نوار اطلاع رسانی <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-info"></i> نوار اطلاع‌رسانی <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="{{route('alerts.create')}}">ایجاد نوار اطلاع رسانی</a></li>
-                        <li><a href="{{route('alerts.index')}}">مدیریت نوارهای اطلاع رسانی</a></li>
+                        <li><a href="{{route('alerts.create')}}">ایجاد نوار اطلاع‌رسانی</a></li>
+                        <li><a href="{{route('alerts.index')}}">مدیریت نوارهای اطلاع‌رسانی</a></li>
                     </ul>
                 </li>
             @can('display-coupons')
-                <li><a><i class="fa fa-percent"></i> کد تخفیف <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-percent"></i> جشنواره و کد تخفیف <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="{{route('coupons.create')}}">ایجاد کد تخفیف</a></li>
-                        <li><a href="{{route('coupons.index')}}">مدیریت کد تخفیف ها</a></li>
-                    </ul>
-                </li>
-            @endcan
-            @can('display-offers')
-                <li><a><i class="fa fa-gift"></i> تخفیف پیشنهادی <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                        <li><a href="{{route('offers.create')}}">ایجاد تخفیف پیشنهادی</a></li>
-                        <li><a href="{{route('offers.index')}}">مدیریت تخفیف پیشنهادی</a></li>
+                        <li><a href="{{route('coupons.index')}}">ایجاد و مدیریت کدهای تخفیف</a></li>
+                        <li><a href="{{route('offers.index')}}">ایجاد و مدیریت جشنواره‌های تخفیف</a></li>
                     </ul>
                 </li>
             @endcan
@@ -150,11 +126,11 @@
                 </li>
             @endcan
             @can('display-payments')
-                <li><a><i class="fa fa-money"></i> لیست پرداخت ها <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-money"></i> لیست پرداخت‌ها <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="{{route('payments.index')}}">همه پرداخت ها</a></li>
-                        <li><a href="{{route('payments.index',['status_code'=>true])}}">پرداخت های موفق</a></li>
-                        <li><a href="{{route('payments.index',['status_code'=>false])}}">پرداخت های ناموفق</a></li>
+                        <li><a href="{{route('payments.index')}}">همه پرداخت‌ها</a></li>
+                        <li><a href="{{route('payments.index',['status_code'=>true])}}">پرداخت‌های موفق</a></li>
+                        <li><a href="{{route('payments.index',['status_code'=>false])}}">پرداخت‌های ناموفق</a></li>
                     </ul>
                 </li>
             @endcan
