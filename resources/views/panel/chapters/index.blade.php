82cad92032247f04a5e7b5e8f0ea11edafc70b89
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-11 21:03:16 +0430

    Add Create Chapter Link in Top of Chapters list in panel/chapters/index.blade.php

--- resources/views/panel/chapters/index.blade.php
+++ resources/views/panel/chapters/index.blade.php
@@ -4,7 +4,7 @@
             <div class="page-title">
                 <div class="title_left">
                     <h3>
-                        <small>لیست فصل‌ها</small>
+                        <a class="btn btn-primary" href="{{route('chapters.create')}}"><i class="fa fa-plus"></i> ایجاد فصل جدید</a>
                     </h3>
                 </div>
 
@@ -25,7 +25,7 @@
                 <div class="x_panel">
                     <div class="x_title">
                         <h2>
-                            <small>فصل‌های دوره</small>
+                            فصل‌های دوره
                         </h2>
                         <ul class="nav navbar-right panel_toolbox">
                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
