3cabff894a94399bfaae16408c6b1496091844dd
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-11 20:58:12 +0430

    Add Create Coupon Link in Top of Coupons list in panel/coupons/index.blade.php

--- resources/views/panel/coupons/index.blade.php
+++ resources/views/panel/coupons/index.blade.php
@@ -4,7 +4,7 @@
             <div class="page-title">
                 <div class="title_left">
                     <h3>
-                        <small>کدهای تخفیف</small>
+                        <a class="btn btn-primary" href="{{route('coupons.create')}}"><i class="fa fa-plus"></i> ایجاد کد تخفیف جدید</a>
                     </h3>
                 </div>
 
@@ -26,7 +26,7 @@
                     <div class="x_panel">
                         <div class="x_title">
                             <h2>
-                                <small>لیست کدهای تخفیف</small>
+                                لیست کدهای تخفیف
                             </h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
