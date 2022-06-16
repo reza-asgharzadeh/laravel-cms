24d91c72089d48a81ff664d9f23e0375ca0a998c
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-16 14:46:24 +0430

    Add migrations/create_faq_courses_table.php

---
+++ database/migrations/2022_06_11_211024_create_faq_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFaqCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faq_courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->tinyText('answer');
+            $table->foreignId('course_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faq_courses');
+    }
+};
