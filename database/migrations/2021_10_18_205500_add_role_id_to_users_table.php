f929c7e3e9c3a71d1fce85230ee0c04f1f9a8171
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-17 00:43:11 +0430

    Change default 3 to 2 in migrations/add_role_id_to_users_table.php

--- database/migrations/2021_10_18_205500_add_role_id_to_users_table.php
+++ database/migrations/2021_10_18_205500_add_role_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddRoleIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->default(3)->constrained()
+            $table->foreignId('role_id')->default(2)->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
         });
