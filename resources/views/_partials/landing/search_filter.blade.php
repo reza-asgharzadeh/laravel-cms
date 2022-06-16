573ed40d0515d23930acf7e1fc1f6f72386fb6d8
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-16 14:27:02 +0430

    Add discuss search result in _partials/landing/search_filter.blade.php

--- resources/views/_partials/landing/search_filter.blade.php
+++ resources/views/_partials/landing/search_filter.blade.php
@@ -4,4 +4,5 @@
 <div class="d-flex bg-light shadow rounded-3 p-2 mb-3">
     <a class="mx-2 rounded-3 text-muted p-2 {{request('type') == 'post' ? 'active-search' : ''}}" href="{{route('search',['search'=>request('search'),'type'=>'post'])}}"><i class="fa fa-lg fa-book mx-2"></i>مقالات</a>
     <a class="mx-2 rounded-3 text-muted p-2 {{request('type') == 'course' ? 'active-search' : ''}}" href="{{route('search',['search'=>request('search'),'type'=>'course'])}}"><i class="fa fa-lg fa-graduation-cap mx-2"></i>دوره‌ها</a>
+    <a class="mx-2 rounded-3 text-muted p-2 {{request('type') == 'discuss' ? 'active-search' : ''}}" href="{{route('search',['search'=>request('search'),'type'=>'discuss'])}}"><i class="fa fa-lg fa-question-circle mx-2"></i>بحث و گفتگوها</a>
 </div>
