4777e2136d1b1d61d186a1f8ae5071048535b69f
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-16 14:18:05 +0430

    Add ->string('slug') in migrations/create_questions_table.php

--- database/migrations/2021_12_25_125454_create_questions_table.php
+++ database/migrations/2021_12_25_125454_create_questions_table.php
@@ -16,6 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->longText('content');
             $table->foreignId('user_id')->constrained()
                 ->onUpdate('CASCADE')
