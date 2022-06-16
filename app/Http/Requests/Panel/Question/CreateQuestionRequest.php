bb461d9bb0491113e86f1230b02f4a1cca5971d5
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-16 14:11:38 +0430

    Add 'string' to content rules in Requests/Panel/Question/CreateQuestionRequest.php

--- app/Http/Requests/Panel/Question/CreateQuestionRequest.php
+++ app/Http/Requests/Panel/Question/CreateQuestionRequest.php
@@ -25,7 +25,7 @@ class CreateQuestionRequest extends FormRequest
     {
         return [
             'title' => ['required','string', 'max:255'],
-            'content' => ['required']
+            'content' => ['required','string']
         ];
     }
 }
