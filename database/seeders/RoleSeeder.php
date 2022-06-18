e0827306b8e11bcec0e5b069dfc5b7f09f9b15b2
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-18 15:18:01 +0430

    Remove  role and Change user permissions in database/seeders/RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -22,18 +22,12 @@ class RoleSeeder extends Seeder
 
         $superAdmin->permissions()->sync(Permission::all());
 
-        $admin = Role::create([
-            'name' => 'admin'
-        ]);
-
-        $admin->permissions()->sync(Permission::all());
-
         $user = Role::create([
            'name' => 'user'
         ]);
 
         $user->permissions()->sync([
-            1,2,3,4,5,11,12,13,14,15,16,17,18,76,77,78
+            1,2,3,4,5,20,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,159,160
         ]);
     }
 }
