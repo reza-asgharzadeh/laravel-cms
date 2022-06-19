ebdbad5ddf84d7d14d0c1765e13c1a527662578b
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-19 21:34:40 +0430

    Add label to rules and Change max:255 to max:40 in Panel/Permission/UpdatePermissionRequest.php

--- app/Http/Requests/Panel/Permission/UpdatePermissionRequest.php
+++ app/Http/Requests/Panel/Permission/UpdatePermissionRequest.php
@@ -27,7 +27,8 @@ class UpdatePermissionRequest extends FormRequest
         $permission = $this->route('permission');
 
         return [
-            'name' => ['required','string', 'max:255', Rule::unique('permissions')->ignore($permission->id)]
+            'name' => ['required','string', 'max:40', Rule::unique('permissions')->ignore($permission->id)],
+            'label' => ['required','string', 'max:40', Rule::unique('permissions')->ignore($permission->id)]
         ];
     }
 }
