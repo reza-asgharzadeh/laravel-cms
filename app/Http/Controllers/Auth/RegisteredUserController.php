efbf92209b51ab9e39802dbfac125de60d7939ca
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-19 20:01:09 +0430

    Add regex to email and mobile in Auth/RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,8 +38,8 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'mobile' => ['required', 'numeric', 'digits:11', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'regex:/[a-z0-9_.]+@((yahoo|gmail)\.com)/' ,'max:255', 'unique:users'],
+            'mobile' => ['required', 'numeric', 'digits:11', 'regex:/09([0-9])/' ,'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'g-recaptcha-response' => [new recaptcha()],
         ]);
