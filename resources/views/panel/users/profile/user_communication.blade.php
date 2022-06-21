1f14c8672411f3d7158574d6d4b9cbff537373c0
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-21 08:46:14 +0430

    Change @include('_partials.panel.profile_tabs') to @include('_partials.panel.edit_profile_tabs') and Remove <small></small> in panel/users/profile/user_communication.blade.php

--- resources/views/panel/users/profile/user_communication.blade.php
+++ resources/views/panel/users/profile/user_communication.blade.php
@@ -24,12 +24,12 @@
             </div>
             <div class="clearfix"></div>
             <div class="row">
-                @include('_partials.panel.profile_tabs')
+                @include('_partials.panel.edit_profile_tabs')
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
                             <h2>
-                                <small>ویرایش پروفایل</small>
+                                ویرایش راه‌های ارتباطی
                             </h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
