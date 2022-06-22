6a0f29a6afd04a53619b0644761f86b989a5610e
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-22 23:36:28 +0430

    Change DataType and Add softDeletes() in migrations/create_permissions_table.php

--- database/migrations/2021_10_16_091039_create_permissions_table.php
+++ database/migrations/2021_10_16_091039_create_permissions_table.php
@@ -15,9 +15,10 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('label')->unique();
+            $table->string('name',70)->unique();
+            $table->string('label',70)->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
