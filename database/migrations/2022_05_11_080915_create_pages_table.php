7404db3d8866436a5eb8b2603c08e6718258bbdd
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-22 23:51:29 +0430

    Change DataType and Add softDeletes() in migrations/create_pages_table.php

--- database/migrations/2022_05_11_080915_create_pages_table.php
+++ database/migrations/2022_05_11_080915_create_pages_table.php
@@ -15,15 +15,16 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
-            $table->string('keywords');
-            $table->string('slug')->unique();
-            $table->string('banner');
-            $table->string('img_alt');
-            $table->longText('content');
+            $table->string('title',200);
+            $table->string('description',255);
+            $table->string('keywords',200);
+            $table->string('slug',200)->unique();
+            $table->string('banner',150);
+            $table->string('img_alt',150);
+            $table->mediumText('content');
             $table->boolean('is_approved');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
