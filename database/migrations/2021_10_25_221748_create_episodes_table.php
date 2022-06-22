0e24dc2526aa7213c0363c0c671a769703fac6ef
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-22 23:41:59 +0430

    Change DataType and Add softDeletes() in migrations/create_episodes_table.php

--- database/migrations/2021_10_25_221748_create_episodes_table.php
+++ database/migrations/2021_10_25_221748_create_episodes_table.php
@@ -15,14 +15,15 @@ class CreateEpisodesTable extends Migration
     {
         Schema::create('episodes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->string('downloadUrl')->unique();
-            $table->string('description');
-            $table->string('keywords');
-            $table->string('time');
+            $table->string('name',200);
+            $table->string('slug',200)->unique();
+            $table->string('downloadUrl',200)->unique();
+            $table->string('description',255);
+            $table->string('keywords',200);
+            $table->string('time',9);
             $table->boolean('display');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
