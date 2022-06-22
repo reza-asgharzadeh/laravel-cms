a8b1568124b0aad00c59daea713b377b3991dcf2
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-22 23:45:08 +0430

    Change DataType and Add softDeletes() in migrations/create_taggables_table.php

--- database/migrations/2021_11_01_183325_create_taggables_table.php
+++ database/migrations/2021_11_01_183325_create_taggables_table.php
@@ -14,9 +14,9 @@ class CreateTaggablesTable extends Migration
     public function up()
     {
         Schema::create('taggables', function (Blueprint $table) {
-            $table->integer('tag_id');
-            $table->integer('taggable_id');
-            $table->string('taggable_type');
+            $table->unsignedSmallInteger('tag_id');
+            $table->unsignedSmallInteger('taggable_id');
+            $table->string('taggable_type',70);
         });
     }
 
