7a71fbb73c76e55d60806f338db152df50d37396
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-23 23:02:24 +0430

    Change Data Type and max string in Panel/Contact/CreateContactRequest.php

--- app/Http/Requests/Panel/Contact/CreateContactRequest.php
+++ app/Http/Requests/Panel/Contact/CreateContactRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Panel\Contact;
 
+use App\Rules\recaptcha;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateContactRequest extends FormRequest
@@ -24,9 +25,10 @@ class CreateContactRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255'],
-            'content' => ['required']
+            'name' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'max:100'],
+            'content' => ['required','string','max:30000'],
+            'g-recaptcha-response' => [new recaptcha()]
         ];
     }
 }
