cf6830ca5e77b43fef328934aa0c704ff0d4bc72
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-23 22:59:21 +0430

    Change Data Type and max string in Panel/Chapter/UpdateChapterRequest.php

--- app/Http/Requests/Panel/Chapter/UpdateChapterRequest.php
+++ app/Http/Requests/Panel/Chapter/UpdateChapterRequest.php
@@ -24,7 +24,7 @@ class UpdateChapterRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:200'],
             'course_id' => ['required','integer']
         ];
     }
