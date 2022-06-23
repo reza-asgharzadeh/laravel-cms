8463ae86f12391dd8456a8e91993fcfe7acd8bae
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-23 23:05:29 +0430

    Change Data Type and max string in Panel/Course/UpdateCourseRequest.php

--- app/Http/Requests/Panel/Course/UpdateCourseRequest.php
+++ app/Http/Requests/Panel/Course/UpdateCourseRequest.php
@@ -26,23 +26,23 @@ class UpdateCourseRequest extends FormRequest
     {
         $course = $this->route('course');
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255', Rule::unique('courses')->ignore($course->id)],
+            'name' => ['required', 'string', 'max:200'],
+            'slug' => ['required', 'string', 'max:200', Rule::unique('courses')->ignore($course->id)],
             'description' => ['required', 'string', 'max:255'],
-            'keywords' => ['required', 'string', 'max:255'],
+            'keywords' => ['required', 'string', 'max:200'],
             'tags' => ['nullable', 'array'],
             'tags.*' => ['nullable', 'string'],
             'categories' => ['required', 'array'],
             'categories.*' => ['required', 'string'],
             'banner' => ['nullable', 'image', 'max:2024'],
-            'img_alt' => ['required', 'string', 'max:255'],
+            'img_alt' => ['required', 'string', 'max:150'],
             'price' => ['required', 'integer'],
-            'pre_course' => ['required', 'string', 'max:255'],
-            'time' => ['required', 'string', 'max:255'],
+            'pre_course' => ['required', 'string', 'max:200'],
+            'time' => ['required', 'string', 'max:9'],
             'course_status' => ['required', 'boolean'],
             'course_level' => ['required', 'integer'],
             'offer_id' => ['nullable', 'integer'],
-            'content' => ['required'],
+            'content' => ['required','string'],
             'is_approved' => ['required','boolean']
         ];
     }
