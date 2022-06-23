cd3cd61e5eca4302ffd6b27150c71eeb08c58956
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-23 23:14:22 +0430

    Change Data Type and max string in Panel/Role/CreateRoleRequest.php

--- app/Http/Requests/Panel/Role/CreateRoleRequest.php
+++ app/Http/Requests/Panel/Role/CreateRoleRequest.php
@@ -24,8 +24,8 @@ class CreateRoleRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required','string', 'max:20', 'unique:roles'],
-            'label' => ['required','string', 'max:20', 'unique:roles']
+            'name' => ['required','string', 'max:50', 'unique:roles'],
+            'label' => ['required','string', 'max:50', 'unique:roles']
         ];
     }
 }
