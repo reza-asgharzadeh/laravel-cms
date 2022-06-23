64320a5b3889c8469f8a4d069f60513c9351bca1
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-23 23:05:01 +0430

    Change Data Type and max string in Panel/Course/CreateCourseRequest.php

--- app/Http/Requests/Panel/Course/CreateCourseRequest.php
+++ app/Http/Requests/Panel/Course/CreateCourseRequest.php
@@ -24,23 +24,23 @@ class CreateCourseRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255', 'unique:courses'],
+            'name' => ['required', 'string', 'max:200'],
+            'slug' => ['required', 'string', 'max:200', 'unique:courses'],
             'description' => ['required', 'string', 'max:255'],
-            'keywords' => ['required', 'string', 'max:255'],
+            'keywords' => ['required', 'string', 'max:200'],
             'tags' => ['required', 'array'],
             'tags.*' => ['required', 'string'],
             'categories' => ['required', 'array'],
             'categories.*' => ['required', 'string'],
             'banner' => ['required', 'image', 'max:2024'],
-            'img_alt' => ['required', 'string', 'max:255'],
+            'img_alt' => ['required', 'string', 'max:150'],
             'price' => ['required', 'integer'],
-            'pre_course' => ['required', 'string', 'max:255'],
-            'time' => ['required', 'string', 'max:255'],
+            'pre_course' => ['required', 'string', 'max:200'],
+            'time' => ['required', 'string', 'max:9'],
             'course_status' => ['required', 'boolean'],
             'course_level' => ['required', 'integer'],
             'offer_id' => ['nullable', 'integer'],
-            'content' => ['required'],
+            'content' => ['required','string'],
             'is_approved' => ['required','boolean']
         ];
     }
