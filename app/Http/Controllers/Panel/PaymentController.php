a9778c0554064328b120f0a6271059c26c15efe1
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-25 21:02:41 +0430

    Remove all Method except index(), storeSuccessRequest() and updateSuccessCallBack() in Panel/PaymentController.php

--- app/Http/Controllers/Panel/PaymentController.php
+++ app/Http/Controllers/Panel/PaymentController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
 use App\Models\Payment;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -12,59 +13,33 @@ class PaymentController extends Controller
     {
         Gate::authorize('view-payments');
 
-        if(isset($request->status_code)) {
-            $payments = match ($request->status_code) {
-                '0' => Payment::where('status_code', '!=' ,100)->paginate(10),
-                '1' => Payment::where('status_code',100)->paginate(10),
+        if(isset($request->status)) {
+            $payments = match ($request->status) {
+                '0' => Payment::where('status', 0)->paginate(10),
+                '1' => Payment::where('status',1)->paginate(10),
             };
-        } else {
-            $payments = Payment::paginate(10);
+            return view('panel.payments.index',compact('payments'));
         }
 
+        $payments = Payment::paginate(10);
         return view('panel.payments.index',compact('payments'));
     }
 
-    public static function storeSuccessRequest($result)
+    public static function storeSuccessRequest($invoice,$dataModel,$transactionId)
     {
-        Payment::create([
-            'order_id' => OrderController::$order->id,
-            'authority' => $result['data']["authority"],
-            'RefID' => null,
-            'gateway_name' => 'zarinpal',
-            'status_code' => $result['data']['code']
+        $dataModel->payments()->create([
+            'transaction_id' => $transactionId,
+            'total' => $invoice->getAmount(),
+            'gateway_name' => $invoice->getDriver(),
         ]);
     }
 
-    public static function storeErrorRequest($result)
+    public static function updateSuccessCallBack($record,$receipt)
     {
-        Payment::create([
-            'order_id' => OrderController::$order->id,
-            'authority' => $result['data']["authority"],
-            'RefID' => null,
-            'gateway_name' => 'zarinpal',
-            'status_code' => $result['errors']['code']
-        ]);
-    }
-
-    public static function storeSuccessCallBack($result,$Authority)
-    {
-        Payment::create([
-            'order_id' => OrderController::$order->id,
-            'authority' => $Authority,
-            'RefID' => $result['data']['ref_id'],
-            'gateway_name' => 'zarinpal',
-            'status_code' => $result['data']['code'],
-        ]);
-    }
-
-    public static function storeErrorCallBack($result,$Authority)
-    {
-        Payment::create([
-            'order_id' => OrderController::$order->id,
-            'authority' => $Authority,
-            'RefID' => $result['data']['ref_id'],
-            'gateway_name' => 'zarinpal',
-            'status_code' => $result['errors']['code'],
+        $record->update([
+            'RefID' => $receipt->getReferenceId(),
+            'status' => 1,
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
     }
 }
