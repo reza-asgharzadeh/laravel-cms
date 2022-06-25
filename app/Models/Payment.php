8febcf26a9a4f087497c3cc01fcca91c75efd139
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-25 21:13:29 +0430

    Change some  column and Remove belongsTo user() Relationship and Add morphTo paymentable() Relationship, Change statusCode(), Add gatewayName() Methods in Payment.php Model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -11,16 +11,17 @@ class Payment extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'order_id',
-        'authority',
+        'transaction_id',
         'RefID',
+        'total',
         'payment_type',
         'gateway_name',
-        'status_code'
+        'status'
     ];
 
-    public function user(){
-        return $this->belongsTo(User::class);
+    public function paymentable()
+    {
+        return $this->morphTo();
     }
 
     public function order(){
@@ -28,27 +29,9 @@ class Payment extends Model
     }
 
     public function statusCode(){
-        $message = match ($this->status_code) {
-            '-9' => 'خطای اعتبار سنجی',
-            '-10' => 'ای پی و يا مرچنت كد پذيرنده صحيح نيست',
-            '-11' => 'مرچنت کد فعال نیست لطفا با تیم پشتیبانی ما تماس بگیرید',
-            '-12' => 'تلاش بیش از حد در یک بازه زمانی کوتاه.',
-            '-15' => 'ترمینال شما به حالت تعلیق در آمده با تیم پشتیبانی تماس بگیرید',
-            '-16' => 'سطح تاييد پذيرنده پايين تر از سطح نقره اي است.',
-            '100' => 'عملیات موفق',
-            '-30' => 'اجازه دسترسی به تسویه اشتراکی شناور ندارید',
-            '-31' => 'حساب بانکی تسویه را به پنل اضافه کنید مقادیر وارد شده واسه تسهیم درست نیست',
-            '-32' => 'دستمزدها معتبر نیست، کل دستمزدها (شناور) حداکثر مقدار اضافه بار بوده است.',
-            '-33' => 'درصد های وارد شده درست نیست',
-            '-34' => 'مبلغ از کل تراکنش بیشتر است',
-            '-35' => 'تعداد افراد دریافت کننده تسهیم بیش از حد مجاز است',
-            '-40' => 'پارامترهای اضافی نامعتبر، expire_in معتبر نیست.',
-            '-50' => 'مبلغ پرداخت شده با مقدار مبلغ در وریفای متفاوت است',
-            '-51' => 'پرداخت ناموفق',
-            '-52' => 'خطای غیر منتظره با پشتیبانی تماس بگیرید',
-            '-53' => 'اتوریتی برای این مرچنت کد نیست',
-            '-54' => 'اتوریتی نامعتبر است',
-            '101' => 'تراکنش وریفای شده',
+        $message = match ($this->status) {
+            0  => 'پرداخت نشده',
+            1 => 'پرداخت شده',
         };
         return $message;
     }
@@ -59,6 +42,14 @@ class Payment extends Model
             'card' => 'کارت به کارت',
             'wallet' => 'کیف پول',
             'cash' => 'نقدی',
+        };
+        return $message;
+    }
+
+    public function gatewayName(){
+        $message = match ($this->gateway_name) {
+            'zarinpal' => 'زرین پال',
+            default => 'نا مشخص'
         };
         return $message;
     }
