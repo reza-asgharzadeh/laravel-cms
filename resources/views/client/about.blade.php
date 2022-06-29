1a52dbb048b91e493aeb9933da85ac19d40fea1f
Reza Asgharzadeh <rezaasgharzadeh73@gmail.com>
2022-06-29 11:26:01 +0430

    responsive about in client/about.blade.php

--- resources/views/client/about.blade.php
+++ resources/views/client/about.blade.php
@@ -1,4 +1,8 @@
-<x-landing-layout>
-    <h2>صفحه درباره ما</h2>
-    <p>بزودی</p>
+<x-landing-layout title="درباره ما"
+                  description="در این قسمت توضیحاتی درباره هدف وب‌سایت دولوپر شو را می‌توانید مشاهده کنید."
+                  keywords="درباره ما"
+                  pageUrl="{{route('about')}}">
+    <div class="container mt-5 bg-light rounded-3">
+        <p>صفحه درباره ما بزودی تکمیل می‌شود.</p>
+    </div>
 </x-landing-layout>
